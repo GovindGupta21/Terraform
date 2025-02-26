4809d06b20692ded7932b11726b548ca79783008
GovindGupta21 <govindarajumurarishetty@gmail.com>
2025-02-26 16:52:11 +0530

    day13

---
+++ Day-13-count-foreach/for-each/main.tf
@@ -0,0 +1,24 @@
+variable "ami" {
+  type    = string
+  default = "ami-0c50b6f7dc3701ddd"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "env" {
+  type    = list(string)
+  default = ["dev", "prod"]
+}
+resource "aws_instance" "sandbox" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  for_each      = toset(var.env)
+#   count = length(var.env)
+
+  tags = {
+    Name = each.value # for a set, each.value and each.key is the same
+  }
+}
